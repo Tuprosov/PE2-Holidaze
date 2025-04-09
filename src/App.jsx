6586692ac4c2e4c122b8ffbbbccf4a7120e3fd7a
Shahzod <shahzodwill@gmail.com>
2025-04-09 09:41:58 +0200

    create routes of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="venue/:id" element={<Venuepage />} />
+        <Route path="wishlist" element={<Wishlistpage />} />
+        <Route path="trips" element={<Tripspage />} />
+        <Route path="userVenues" element={<userVenues />} />
+        <Route path="createListing" element={<createListing />} />
+        <Route path="profile" element={<Profilepage />} />
+        <Route path="auth" element={<Authpage />} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default App;
