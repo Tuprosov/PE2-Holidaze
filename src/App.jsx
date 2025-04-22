519e54914267cf07ec818d3f249e270a6b1e6ce5
Shahzod <shahzodwill@gmail.com>
2025-04-22 17:00:26 +0200

    create route for venue page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/Home";
+import VenuePage from "./pages/DisplayVenue";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Homepage />} />
-        <Route path="venue/:id" element={<Venuepage />} />
-        <Route path="wishlist" element={<Wishlistpage />} />
-        <Route path="trips" element={<Tripspage />} />
-        <Route path="userVenues" element={<userVenues />} />
-        <Route path="createListing" element={<createListing />} />
-        <Route path="profile" element={<Profilepage />} />
-        <Route path="auth" element={<Authpage />} />
+        <Route index element={<HomePage />} />
+        <Route path="venue/:id" element={<VenuePage />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+
+{
+  /* <Route index element={<Homepage />} />
+        <Route path="venue/:id" element={<Venuepage />} />
+        <Route path="wishlist" element={<Wishlistpage />} />
+        <Route path="trips" element={<Tripspage />} />
+        <Route path="userVenues" element={<userVenues />} />
+        <Route path="createListing" element={<createListing />} />
+        <Route path="profile" element={<Profilepage />} />
+        <Route path="auth" element={<Authpage />} /> */
+}
