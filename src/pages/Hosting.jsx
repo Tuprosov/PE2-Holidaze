b7b6bb4b96d206ac066da95bfce9ce7a9660c023
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:59:07 +0200

    refactor HostingPage component for improved loading state and user authorization handling

--- src/pages/Hosting.jsx
+++ src/pages/Hosting.jsx
@@ -1,39 +1,58 @@
 import Header from "../components/hostingPage/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/global/Footer";
 import Main from "../components/hostingPage/Main";
 import { useUserStore } from "../js/store/userStore";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getTitle } from "../js/utils/generateTitles";
 
 export default function HostingPage() {
-  const { getVenues, user, setMessage, message, setIsLoading } = useUserStore();
+  const { getVenues, user, setMessage, message, isLoading, isLoggedIn } =
+    useUserStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (!user) return;
     const fetchData = async () => {
       try {
         await getVenues(user.name);
-        setIsLoading(false);
       } catch (error) {
         setMessage(error.message || "Something went wrong");
-        setIsLoading(false);
       }
     };
+    document.title = getTitle(location.pathname, { user });
     fetchData();
-  }, [getVenues]);
+  }, [user]);
 
-  if (message) {
+  if (isLoading) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-2xl font-bold mb-4">{message}</h1>
-        <p className="text-lg text-gray-700">Please try again later.</p>
+      <div className="flex justify-center items-center h-screen">
+        <p>Loading...</p>
       </div>
     );
   }
 
-  return (
+  return isLoggedIn ? (
     <>
       <Header />
-      <div>
+      {message ? (
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold mb-4">{message}</h1>
+          <p className="text-lg text-gray-700">Please try again later.</p>
+        </div>
+      ) : (
         <Main />
+      )}
+      <Footer />
+    </>
+  ) : (
+    <>
+      <Header />
+      <div className="flex h-dvh justify-center items-center">
+        <h1>You’re not authorized. Please login or sign up</h1>
+        <button className="p-2" onClick={() => navigate("/auth")} type="button">
+          Login/Sign up
+        </button>
       </div>
       <Footer />
     </>
