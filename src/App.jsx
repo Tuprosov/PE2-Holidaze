21e1b5fda69307234885fbf206ec02a4f2461fce
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:54:31 +0200

    create route for hosting and listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/global/Layout";
 import HomePage from "./pages/Home";
 import VenuePage from "./pages/DisplayVenue";
 import AuthPage from "./pages/Auth";
 import TripsPage from "./pages/Trips";
 import AccountPage from "./pages/Account";
 import ProfilePage from "./pages/Profile";
+import HostingPage from "./pages/Hosting";
+import ListingPage from "./pages/Listing";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="profile" element={<ProfilePage />} />
       </Route>
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/hosting" element={<HostingPage />} />
+      <Route path="/myVenues" element={<ListingPage />} />
     </Routes>
   );
 }
