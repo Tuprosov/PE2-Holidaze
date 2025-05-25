68f70d1052ee42b269d7ed9896ae1e90fd8866e8
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:58:43 +0200

    refactor AccountPage component to manage user authentication and navigation

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,19 @@
 import DisplayAccount from "../components/accountPage/displayAccount";
+import { useUserStore } from "../js/store/userStore";
+import { useNavigate } from "react-router-dom";
 
-export default function Account() {
-  return <DisplayAccount />;
+export default function AccountPage() {
+  const { isLoggedIn } = useUserStore();
+  const navigate = useNavigate();
+
+  return isLoggedIn ? (
+    <DisplayAccount />
+  ) : (
+    <div className="flex h-dvh justify-center items-center">
+      <h1>You’re not authorized. Please login or sign up</h1>
+      <button className="p-2" onClick={() => navigate("/auth")} type="button">
+        Login/Sign up
+      </button>
+    </div>
+  );
 }
