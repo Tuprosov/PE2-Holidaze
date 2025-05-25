201afacbaaa42bad12244757310aa309d1ad6df2
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:58:09 +0200

    refactor constants.js for improved API_BASE structure and endpoint definitions

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,13 @@
 export const API_KEY = "9b9279a4-5aae-4428-bb34-ce6cfb24d805";
 
-export const API_BASE = "https://v2.api.noroff.dev/holidaze/venues";
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_HOLIDAZE_VENUES = `${API_BASE}/holidaze/venues`;
+
+export const API_HOLIDAZE_BOOKINGS = `${API_BASE}/holidaze/bookings`;
+
+export const API_HOLIDAZE_PROFILES = `${API_BASE}/holidaze/profiles`;
+
+export const API_AUTH_LOGIN = `${API_BASE}/auth/login`;
+
+export const API_AUTH_REGISTER = `${API_BASE}/auth/register`;
