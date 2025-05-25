4770bb7111f8fbdaea2d3628158d2ab3571101a0
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:57:47 +0200

    refactor Nav component for improved layout and link structure

--- src/components/hostingPage/Nav.jsx
+++ src/components/hostingPage/Nav.jsx
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex gap-4 text-gray-700">
+    <nav className="flex justify-center">
+      <ul className="flex flex-wrap md:flex-nowrap gap-4 text-gray-700">
         <li>
-          <Link
-            to="/userVenues"
-            className="block p-2 rounded hover:bg-gray-100"
-          >
+          <Link to="/myVenues" className="block p-2 rounded hover:bg-gray-100">
             Listings
           </Link>
         </li>
         <li>
           <Link to="/messages" className="block p-2 rounded hover:bg-gray-100">
             Messages
+          </Link>
+        </li>
+        <li>
+          <Link to="/hosting" className="block p-2 rounded hover:bg-gray-100">
+            Reservations
           </Link>
         </li>
         <li>
