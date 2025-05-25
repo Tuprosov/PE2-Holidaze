aa3dfcb08bf805b7fa62ae5d071f21238c86a548
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:56:19 +0200

    add Layout component for page structure with dynamic title management

---
+++ src/components/global/Layout.jsx
@@ -0,0 +1,26 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import { Outlet, useLocation } from "react-router-dom";
+import { getTitle } from "../../js/utils/generateTitles.js";
+import { useEffect } from "react";
+import { useUserStore } from "../../js/store/userStore.js";
+import { useVenueStore } from "../../js/store/useStore.js";
+
+export default function Layout() {
+  const { user } = useUserStore();
+  const { singleVenue } = useVenueStore();
+  const location = useLocation();
+  const isVenuePage = location.pathname.startsWith("/venue/");
+
+  useEffect(() => {
+    document.title = getTitle(location.pathname, { user, singleVenue });
+  }, [location.pathname, singleVenue]);
+
+  return (
+    <div className={isVenuePage ? "max-w-[1280px] mx-auto" : ""}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
