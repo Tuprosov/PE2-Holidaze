2e654606d631e28fa813b0b2c548b86250f16b50
Shahzod <shahzodwill@gmail.com>
2025-05-25 13:57:27 +0200

    refactor Header component for improved layout and logo display

--- src/components/hostingPage/Header.jsx
+++ src/components/hostingPage/Header.jsx
@@ -1,23 +1,31 @@
 import Menu from "./Menu";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
+import { useUserStore } from "../../js/store/userStore";
+import logo from "../..//assets/holidaze-logo.png";
 
 export default function Header() {
+  const { isLoggedIn } = useUserStore();
   return (
     <>
-      <header>
-        <div className="flex justify-between items-centerh-20 mb-10">
-          <div className="logo">
-            <Link className="flex-[1]">
-              <div>Logo</div>
+      <header className="relative mb-10">
+        <div className="flex justify-between items-center h-20 px-4">
+          <div className="flex-[1]">
+            <Link>
+              <div className="max-w-[80px]">
+                <img className="w-full h-auto" src={logo} alt="logo" />
+              </div>
             </Link>
           </div>
-          <div className="nav">
-            <Nav />
+          <div className="hidden md:block flex-[2]">
+            {isLoggedIn && <Nav />}
           </div>
-          <div className="relative">
+          <div className="flex-[1] flex justify-end">
             <Menu />
           </div>
+        </div>
+        <div id="hostingNav" className="block md:hidden top-30 left-0">
+          {isLoggedIn && <Nav />}
         </div>
       </header>
     </>
