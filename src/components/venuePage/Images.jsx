0d9cd06afcf40edb1b5b8bd3f59eac0d59717cc3
Shahzod <shahzodwill@gmail.com>
2025-05-27 12:51:32 +0200

    refactor Images component layout and integrate Swiper for mobile slider

--- src/components/venuePage/Images.jsx
+++ src/components/venuePage/Images.jsx
@@ -1,4 +1,9 @@
 import { useVenueStore } from "../../js/store/useStore";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Images() {
   const { singleVenue } = useVenueStore();
@@ -6,7 +11,7 @@ export default function Images() {
 
   const placeholder = "https://placehold.co/40x240?text=No+Image";
 
-  // Ensure there are 5 images total: 1 main + 4 others
+  // there are 5 images total: 1 main + 4 others
   const imagesToShow = [...media];
 
   while (imagesToShow.length < 5) {
@@ -14,26 +19,50 @@ export default function Images() {
   }
 
   return (
-    <div className="w-full grid grid-cols-4 grid-rows-2 gap-2 h-[600px] overflow-hidden rounded-2xl">
-      {/* 1 main image */}
-      <div className="col-span-2 row-span-2">
-        <img
-          src={imagesToShow[0].url}
-          alt="Main venue"
-          className="w-full h-full object-cover rounded-l-2xl"
-        />
-      </div>
-
-      {/* 4 images */}
-      {imagesToShow.slice(1, 5).map((image, idx) => (
-        <div key={idx} className="col-span-1 row-span-1">
+    <>
+      {/* Desktop grid layout (shown on sm and up) */}
+      <div className="hidden sm:grid w-full grid-cols-4 grid-rows-2 gap-2 aspect-[4/3] lg:aspect-[16/9] overflow-hidden rounded-2xl">
+        {/* Main image */}
+        <div className="col-span-2 row-span-2">
           <img
-            src={image.url}
-            alt={image.alt}
-            className="w-full h-full object-cover"
+            src={imagesToShow[0].url}
+            alt="Main venue"
+            className="w-full h-full object-cover rounded-l-2xl"
           />
         </div>
-      ))}
-    </div>
+
+        {/* 4 small images */}
+        {imagesToShow.slice(1, 5).map((image, idx) => (
+          <div key={idx} className="col-span-1 row-span-1">
+            <img
+              src={image.url}
+              alt={image.alt}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Mobile slider (shown below sm) */}
+      <div className="sm:hidden">
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          modules={[Navigation, Pagination]}
+        >
+          {imagesToShow.map((image, idx) => (
+            <SwiperSlide key={idx}>
+              <img
+                src={image.url}
+                alt={image.alt}
+                className="w-full h-[300px] object-cover rounded-2xl"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
   );
 }
