5f1470549b4fca7a2f09285ad0f8117a5699e0a8
Shahzod <shahzodwill@gmail.com>
2025-07-08 15:10:26 +0200

    refactor MainContent component; remove LazyVenueCard and simplify VenueCard rendering logic

--- src/components/homePage/MainContent.jsx
+++ src/components/homePage/MainContent.jsx
@@ -1,32 +1,14 @@
-import { useInView } from "react-intersection-observer";
 import VenueCard from "./VenueCard";
 import { useVenueStore } from "../../js/store/useStore";
-
-function LazyVenueCard({ venue }) {
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Load only once when visible
-    threshold: 0.1, // Trigger when 10% visible
-  });
-
-  return (
-    <div ref={ref} style={{ minHeight: "400px" }}>
-      {inView ? <VenueCard venue={venue} /> : null}
-    </div>
-  );
-}
 
 export default function MainContent() {
   const { venues } = useVenueStore();
 
   return venues.length ? (
     <main className="grid justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 min-h-[100dvh]">
-      {venues.map((venue, index) =>
-        index < 4 ? (
-          <VenueCard key={venue.id} venue={venue} isAboveFold={true} />
-        ) : (
-          <LazyVenueCard key={venue.id} venue={venue} />
-        )
-      )}
+      {venues.map((venue, index) => (
+        <VenueCard key={venue.id} venue={venue} isAboveFold={index < 4} />
+      ))}
     </main>
   ) : (
     <div className="flex justify-center items-center min-h-[100dvh]">
